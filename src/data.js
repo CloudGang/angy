b621960b4b4dd59c23d2b0ca5ae771e64ac1e6fd
cloudgang <35246400+CloudGang@users.noreply.github.com>
2021-01-03 02:03:41 -0500

    -

--- src/data.js
+++ src/data.js
@@ -4,7 +4,6 @@ import t3 from './components/data/t3.jpg'
 import t4 from './components/data/t4.jpg'
 import t5 from './components/data/t5.jpg'
 import t6 from './components/data/t6.jpg'
-import Consent from './components/Consent.jsx'
 
 
 export default [
@@ -17,8 +16,7 @@ export default [
       description:
         "Ah, nostalgia. Give yourself a double dose with this grown-up, just-for-you version of one of childhood's greatest gadgets: the reel viewer. Assemble snapshots of your favorite memories and add them to your very own reel using the redemption code included with your viewer. Once you receive your made-to-order reel, pop it in the viewer and marvel at baby's first steps, or your favorite beach at sunset, or... well, whatever else you want immortalized. Remember: You're the view master now. Made in Oregon.",
       shipping:
-        Consent
-        //"Most of our items are in stock and will ship quickly. Orders for in-stock items placed before 5pm ET Monday through Friday, excluding Federal holidays, will ship in the US: ECONOMY SHIPPING: within 8 days, arriving no later than the 9th business day after the order was placed. STANDARD SHIPPING: within 4 days, arriving 5 business days after the order was placed. PREFERRED SHIPPING: within 2 days, arriving 3 business days after the order was placed. EXPEDITED SHIPPING: within 1 day, arriving 2 business days after the order was placed. EXPRESS SHIPPING: will ship the same day and arrive 1 business day later."
+        "Most of our items are in stock and will ship quickly. Orders for in-stock items placed before 5pm ET Monday through Friday, excluding Federal holidays, will ship in the US: ECONOMY SHIPPING: within 8 days, arriving no later than the 9th business day after the order was placed. STANDARD SHIPPING: within 4 days, arriving 5 business days after the order was placed. PREFERRED SHIPPING: within 2 days, arriving 3 business days after the order was placed. EXPEDITED SHIPPING: within 1 day, arriving 2 business days after the order was placed. EXPRESS SHIPPING: will ship the same day and arrive 1 business day later."
     },
     {
       name: "Set 2",
