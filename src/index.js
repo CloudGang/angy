3d7b287cfc795082816277c332b1774eebd3ca7f
cloudgang <35246400+CloudGang@users.noreply.github.com>
2021-01-04 12:36:59 -0500

    node

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+import LogRocket from 'logrocket';
+//import setupLogRocketReact from 'logrocket-react';
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
+LogRocket.init('lbzpyi/angynails');
+//setupLogRocketReact(LogRocket);
+
 ReactDOM.render(
   <Router>
     <App />
